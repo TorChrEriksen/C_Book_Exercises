8412101594262fb9c3e817f23073fbf7ea32f075
TorChrEriksen <trofast@gmail.com>
2015-08-26 12:36:30 +0000

    Refined xrcise 13.

--- Chapter_1/13/histogram.c
+++ Chapter_1/13/histogram.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // This program prints a histogram of the lengths of words in its input.
-// Words longer than 10 characters are for now listed as 10+.
+// Words longer than 15 characters are for now listed as 15+.
 // Zero length words are just whitespaces, tabs, etc.
 
 // TODO: Does not handle characters like æ,ø,å very well (counts as two characters it seems).
@@ -11,11 +11,11 @@ main()
 	// state = 0 : waiting for word to start.
 	// state > 0 : currently counting a word.
 	int i, state;
-	int nWords[11];
+	int nWords[16];
 
 	state = 0;
 
-	for (i = 0; i < 11; ++i)
+	for (i = 0; i < 16; ++i)
 		nWords[i] = 0;
 
 	i = 0;
@@ -23,10 +23,10 @@ main()
 	while ((i = getchar()) != EOF) {
 		if (i == ' ' || i == '\t' || i == '\n') {
 			
-			if (state > 0 && state < 10)
+			if (state > 0 && state < 15)
 				++nWords[state];
-			else if (state >= 10)
-				++nWords[10];
+			else if (state >= 15)
+				++nWords[15];
 			else if (state < 0)
 				printf("An error occured.");
 			else
@@ -46,25 +46,25 @@ main()
 
 	// Printed histogram should look something in order of this:
 	// 8|                               |
-	// 7|          __                __ |
-	// 6|          __    __          __ |
-	// 5|    __    __    __          __ |
-	// 4|    __ __ __    __    __    __ |
-	// 3| __ __ __ __ __ __    __ __ __ |
-	// 2| __ __ __ __ __ __    __ __ __ |
-	// 1| __ __ __ __ __ __ __ __ __ __ |
+	// 7|          ++                ++ |
+	// 6|          ++    ++          ++ |
+	// 5|    ++    ++    ++          ++ |
+	// 4|    ++ ++ ++    ++    ++    ++ |
+	// 3| ++ ++ ++ ++ ++ ++    ++ ++ ++ |
+	// 2| ++ ++ ++ ++ ++ ++    ++ ++ ++ |
+	// 1| ++ ++ ++ ++ ++ ++ ++ ++ ++ ++ |
 	//--|-------------------------------|
 	// #|  1  2  3  4  5  6  7  8  9 10+
 
 	// Locate the highest word count (ignore whitespaces since they will be very large).
 	int lWord = 0;
-	for (i = 1; i < 11; ++i) {
+	for (i = 1; i < 16; ++i) {
 		if (nWords[i] > lWord)
 			lWord = nWords[i];
 	}
 
-	int k, printArray[11][lWord];
-	for (i = 0; i < 11; ++i) {
+	int k, printArray[16][lWord];
+	for (i = 0; i < 16; ++i) {
 		for (k = 0; k < lWord; ++k) {
 			if (nWords[i] > k )
 				printArray[i][k] = 1;
@@ -76,10 +76,10 @@ main()
 	// Print the histogram represented by printArray[][]
 	// Starting with the highest number so we need to loop backwards through lWord.
 	// We leave all the zero length words out (whitespaces etc).
-	printf("\n%2d|                               |\n", lWord+1);
+	printf("\n%10d|                                              |\n", lWord+1);
 	for (k = lWord-1; k >= 0; --k) {
-		printf("%2d|", k+1);
-		for (i = 1; i < 11; ++i) {
+		printf("%10d|", k+1);
+		for (i = 1; i < 16; ++i) {
 			if (printArray[i][k])
 				printf(" ++");
 			else
@@ -88,6 +88,6 @@ main()
 		printf(" |\n");
 	}
 
-	printf("--|-------------------------------|\n");
-	printf(" #|  1  2  3  4  5  6  7  8  9 10+\n\n");
+	printf("----------|----------------------------------------------|\n");
+	printf("wordlength|  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15+\n\n");
 }
